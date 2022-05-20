82ae0b87be17e698ab0bcc59334a436595e6040d
Hong Zhang <hongzhang@anl.gov>
2022-05-20 11:19:05 -0500

    Clean up the doc

--- pnode/petsc_adjoint.py
+++ pnode/petsc_adjoint.py
@@ -255,7 +255,7 @@ class ODEPetsc(object):
         pass
 
     def tspanPostStep(self, ts):
-        """"Save the solutions at intermediate points"""
+        """Set time step size"""
         stepno = ts.getStepNumber()
         t = ts.getTime()
         if self.cur_sol_index < len(self.sol_times):
@@ -450,6 +450,8 @@ class ODEPetsc(object):
             else:
                 solution = torch.stack([torch.from_numpy(tspan_sols[i].array_r.reshape(self.tensor_size)) for i in range(len(tspan_sols))], dim=0)
             self.ts.setPostStep(None)
+        if self.cur_sol_index != len(self.sol_times):
+            raise Exception("TSSolve fails to step on all the specified points")
         return solution
 
     def petsc_adjointsolve(self, t, i=1):
